2d656731d79663b20ff8a7e62892facbbff2aead
angryhorse <groznykon@gmail.com>
2018-03-11 17:53:13 +0200

    shop

--- js/main.js
+++
@@ -1,7 +0,0 @@
-$('.hero').waypoint(function(direction) {
-  if (direction == "down") {
-    $('.sticky-menu').addClass('gym');
-  } else if (direction == "up") {
-    $('.sticky-menu').removeClass('gym');
-  }
-});
